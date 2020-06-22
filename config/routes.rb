4aac2f4e1835eed25758810fca8509b64ebc5a7a
Megan Thresh <meganthresh96@gmail.com>
2020-06-22 16:08:30 +0100

    MADE CHANGES TO COCKTAIL CONTROLLER AND NEW RECIPES VIEW

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :recipe_amounts, only: [:new, :create]
   end
 
+  resources :cocktails, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
