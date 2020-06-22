aaf8e650580fdb6306f76154f1913fd4a956f51b
Megan Thresh <meganthresh96@gmail.com>
2020-06-22 18:25:12 +0100

    COMPLETED RECIPE ADD INGREDIENT FORM. BASIC STRUCTURE DONE.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :recipe_amounts, only: [:new, :create]
   end
-
-  resources :cocktails, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
